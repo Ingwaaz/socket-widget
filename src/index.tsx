750d72467474a043d7f0a0bbd596d14aa17e002b
Ingwaaz <ingwaazdesign@mail.ru>
2022-10-11 18:18:05 +0300

    test2

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+
+console.log(root);
+
 root.render(
   <React.StrictMode>
     <App />
